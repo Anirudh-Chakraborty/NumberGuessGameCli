e3e771eedf56fc2e5352f8cd08ee6c34973ab12e
anirudhchakraborty <preetiujjal@gmail.com>
2025-11-23 10:35:00 +0530

    Game Created and Completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,40 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-    Main caller = new Main(); // for calling other methods
+        Main caller = new Main(); // for calling other methods
         caller.menu();
 
+        int answer = (int) (Math.random() * (1000 - 10 + 1)) + 10;
+        System.out.println("Make Your 1st guess");
 
+
+        for (int i = 10; i > -1; i--) {
+            int choice = sc.nextInt();
+            boolean camback = caller.Guess(choice,answer,i);
+            if (camback) {
+                System.out.println("Your guess is correct");
+                int j = 10 - i;
+                System.out.println("You dit it in: "+ j + "attempts");
+                break;
+
+            } else if(i>0){
+                System.out.println("Lives remain: " + i);
+            }
+            else {
+                System.out.println("All Lives Exhausted, Bad Luck or Shall I say Bad Guess");
+                System.out.println("Answer was: " + answer);
+                break;
+            }
+        }
     }
 
-    void menu(){
+    void menu() {
         System.out.println("Hello and welcome!");
 
         System.out.println("Lets Play the Num guess game");
@@ -22,5 +47,22 @@ public class Main {
         System.out.println("The Range will be between 10 to 1000");
     }
 
+    boolean Guess(int num, int answer, int i) {
 
+        if (num == answer ) {
+            System.out.println("Congo");
+            return true;
+        }
+        else if (num < answer&& i>0) {
+            System.out.println("Try higher than this");
+            return false;
+        }
+        else if (num > answer&& i>0) {
+            System.out.println("Try lower than this");
+            return false;
+        }
+        else {
+            return false;
+        }
+    }
 }
