706e5b1d5ff7697937ba5b455614f88905fc8a3e
anirudhchakraborty <preetiujjal@gmail.com>
2025-11-25 11:04:28 +0530

    Polished The Game

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,17 +5,50 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 public class Main {
     static Scanner sc = new Scanner(System.in);
-
+    static Main caller = new Main();
     public static void main(String[] args) {
-        Main caller = new Main(); // for calling other methods
+         // for calling other methods
         caller.menu();
+        caller.sendNum();
 
-        int answer = (int) (Math.random() * (1000 - 10 + 1)) + 10;
+    }
+
+    void menu() {
+        System.out.println("Hello and welcome!");
+
+        System.out.println("Lets Play the Num guess game");
+
+        System.out.println("The Rules are ");
+        System.out.println("1. you'll be given 10 lives in which you have to guess the number");
+        System.out.println("2. Each time you miss you'll be given a hint and a heart will be deducted");
+        System.out.println("3. If your heart turn to 0 you lose");
+        System.out.println("4. At start you'll be given a free choice ");
+        System.out.println("The Range will be between 10 to 500");
+    }
+
+    void sendNum (){
+
+        int answer = (int) (Math.random() * (500 - 10 + 1)) + 10;
         System.out.println("Make Your 1st guess");
 
-
         for (int i = 10; i > -1; i--) {
-            int choice = sc.nextInt();
+            int choice;
+            while(true){
+                if (sc.hasNextInt()) {
+                    choice = sc.nextInt();
+
+                    if (10<=choice && choice<=500) {
+                    break;
+                    }
+                    else {
+                        System.out.println("number shall be between 10 and 500");
+                    }
+                } else {
+                    System.out.println("Try again — only numbers!");
+                    sc.next(); // clear the bad input
+                }
+            }
+
             boolean camback = caller.Guess(choice,answer,i);
             if (camback) {
                 System.out.println("Your guess is correct");
@@ -32,19 +65,7 @@ public class Main {
                 break;
             }
         }
-    }
 
-    void menu() {
-        System.out.println("Hello and welcome!");
-
-        System.out.println("Lets Play the Num guess game");
-
-        System.out.println("The Rules are ");
-        System.out.println("1. you'll be given 10 lives in which you have to guess the number");
-        System.out.println("2. Each time you miss you'll be given a hint and a heart will be deducted");
-        System.out.println("3. If your heart turn to 0 you lose");
-        System.out.println("4. At start you'll be given a free choice ");
-        System.out.println("The Range will be between 10 to 1000");
     }
 
     boolean Guess(int num, int answer, int i) {
